55cfaae3133392d41c22cccb355c89f1ea7c9d65
4xL <77083140+4xLoff@users.noreply.github.com>
2024-03-07 05:28:46 -0500

    helpers.sh

--- helpers.sh
+++ helpers.sh
@@ -146,7 +146,7 @@ function other(){
         echo -e "${greenColour}Update system.${endColour}"
         cd /home/$SUDO_USER/Downloads
         echo "console-setup   console-setup/charmap47   select  Guess optimal character set" | sudo debconf-set-selections
-        sudo apt update && sudo apt -y full-upgrade
+        sudo apt update && sudo apt upgrade -y
         echo -e "${greenColour}Additional packages will be installed for the correct functioning of the environment.${endColour}"
         sleep 3
         sudo apt install -y feh scrot scrub zsh rofi xclip locate neofetch acpi bspwm sxhkd imagemagick snap caja kitty ranger i3lock-fancy wmname firejail cmatrix htop python3-pip procps tty-clock fzf pamixer flameshot python3 gcc g++ libfreetype6-dev libglib2.0-dev libcairo2-dev meson pkg-config gtk-doc-tools zlib1g-dev libpng16-16 liblcms2-2 librsync-dev libssl-dev libfreetype6 libfreetype6-dev fontconfig imagemagick ffuf pkg-config libdbus-1-dev libxcursor-dev libxrandr-dev libxi-dev libxinerama-dev libgl1-mesa-dev libxkbcommon-x11-dev libfontconfig1-dev libx11-xcb-dev liblcms2-dev libssl-dev libpython3-dev libharfbuzz-dev wayland-protocols libxxhash-dev bc zsh-syntax-highlighting ranger seclists
