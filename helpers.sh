494cf9d0a37b3fa891eff134f0814085a2c3d53c
4xL <77083140+4xLoff@users.noreply.github.com>
2024-08-16 09:50:38 -0500

    fiz path

--- helpers.sh
+++ helpers.sh
@@ -30,6 +30,7 @@ function check(){
 
 function other(){       
         echo -e "${greenColour}Update system.${endColour}"
+        sudo -u "$SUDO_USER" mkdir -p /home/$SUDO_USER/Downloads
         cd /home/$SUDO_USER/Downloads
         sudo apt update && sudo apt upgrade -y
         sudo apt --fix-broken install
