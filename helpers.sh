4be241e0b8be47cc4dc900846faedc55bfb792c0
4xL <77083140+4xLoff@users.noreply.github.com>
2024-08-17 08:57:00 -0500

    nvim

--- helpers.sh
+++ helpers.sh
@@ -152,7 +152,7 @@ function script(){
         echo -e "${greenColour}Install nvcahd.${endColour}"
         sudo rm -rf /home/$SUDO_USER/.config/nvim
         pushd /opt &>/dev/null && sudo wget -q https://github.com/neovim/neovim/releases/download/v0.9.5/nvim-linux64.tar.gz && sudo tar -xf nvim-linux64.tar.gz; popd &>/dev/null
-        sudo -u "$SUDO_USER "git clone https://github.com/NvChad/starter /HOME/$SUDO_USER/.config/nvim && nvim
+        sudo -u "$SUDO_USER" git clone https://github.com/NvChad/starter /HOME/$SUDO_USER/.config/nvim && nvim
         sudo rm -rf /root/.config/nvim
         sudo git clone https://github.com/NvChad/starter /root/.config/nvim && nvim
         sudo ln -s -f /opt/nvim-linux64/bin/nvim /usr/bin/
